eef898b8fbce0fa735e102e1dc7010a29c762413
caiomoreiradc <caiomoreiracarvalhoo@outlook.com>
2023-03-21 17:43:49 -0300

    Refactored Code

--- SteamKeys/Program.cs
+++ SteamKeys/Program.cs
@@ -5,22 +5,8 @@ namespace SteamKeys
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void SalvarArquivo(Random random, string chars, int keyLength, int qtdKeys, string fileName)
         {
-            Random random = new Random();
-            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            int keyLength = 15;
-
-            Console.WriteLine("Generate Steam Keys!!!!");
-            Console.WriteLine();
-
-            Console.WriteLine("How many Keys do you want to generate? ");
-            int qtdKeys = int.Parse(Console.ReadLine());
-
-            // Nome do arquivo onde as keys serão salvas
-            string fileName = "steamkeys.txt";
-
-            // Cria um StreamWriter para o arquivo de saída
             using (StreamWriter writer = new StreamWriter(fileName))
             {
                 for (int i = 0; i < qtdKeys; i++)
@@ -35,12 +21,34 @@ namespace SteamKeys
                     string key = new string(keyChars);
                     string formattedKey = string.Format("{0}-{1}-{2}", key.Substring(0, 5), key.Substring(5, 5), key.Substring(10, 5));
 
-                    // Escreve a chave formatada no arquivo de saída
                     writer.WriteLine(formattedKey);
                 }
             }
-
-            Console.WriteLine("Keys saved to file: " + fileName);
         }
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            int keyLength = 15;
+
+            Console.WriteLine("Generate Steam Keys!!!!");
+            Console.WriteLine();
+
+            Console.Write("Inset how many keys you want to generate: ");
+            int qtdKeys = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            string fileName = "steamkeys.txt";
+
+            SalvarArquivo(random, chars, keyLength, qtdKeys, fileName);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Keys saved to file: " + fileName);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Press ENTER to exit");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+
     }
 }
